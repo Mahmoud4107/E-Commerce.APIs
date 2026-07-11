368cfb3ff7c8482d7e8b436895cd27a859acaee4
20200492 <mahmoudhashem20112002@gmail.com>
2026-07-11 09:41:22 +0300

    Generic Repository

---
+++ E-Commerce.Core/RepostriesContruct/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using E_Commerce.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.RepostriesContruct
+{
+    public interface IGenericRepository <T> where T : ModelEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<T?> GetByIdAsync(int id);
+
+    }
+}
---
+++ E-Commerce.Repository/GenericRepository.cs
@@ -0,0 +1,30 @@
+﻿using E_Commerce.Core.Entities;
+using E_Commerce.Core.RepostriesContruct;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : ModelEntity
+    {
+        private readonly StoreContext _context;
+
+        public GenericRepository(StoreContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+    }
+}
