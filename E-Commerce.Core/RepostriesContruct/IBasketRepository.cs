161097d121f34f27782441540a75d5878e48fd7b
20200492 <mahmoudhashem20112002@gmail.com>
2026-09-09 03:26:27 +0300

    Basket Repository

---
+++ E-Commerce.Core/RepostriesContruct/IBasketRepository.cs
@@ -0,0 +1,16 @@
+﻿using E_Commerce.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Core.RepostriesContruct
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket?> GetBasketAsync(string id);
+        Task<CustomerBasket?> UpdateBasketAsync(CustomerBasket basket);
+        Task<bool> DeleteBasketAsync(string id);
+    }
+}
